e43327c08c3b0d98f97acf97da7b766a7796e269
dmdev2020 <dmdev2020@gmail.com>
2021-07-24 17:05:05 +0200

    added files from lesson-22

--- src/main/java/com/dmdev/HelloWorld.java
+++ src/main/java/com/dmdev/HelloWorld.java
@@ -1,8 +1,10 @@
 package com.dmdev;
 
+import org.springframework.util.StringUtils;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println(StringUtils.capitalize("hello world"));
     }
 }
