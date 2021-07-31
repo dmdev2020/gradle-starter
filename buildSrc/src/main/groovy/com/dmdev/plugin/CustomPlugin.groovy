da4413bbf529e864637f8931c99fbd1d064dfb71
dmdev2020 <dmdev2020@gmail.com>
2021-07-31 17:21:43 +0200

    added classes for lesson-30

---
+++ buildSrc/src/main/groovy/com/dmdev/plugin/CustomPlugin.groovy
@@ -0,0 +1,32 @@
+package com.dmdev.plugin
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class CustomPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        addHelloTask(project)
+    }
+
+    void addHelloTask(Project project) {
+        def map = [description: "Hello task", group: "dmdev", type: DefaultTask]
+        project.task(map, "hello") {
+            println "configuration phase. Task hello"
+
+            doLast {
+                println "execution phase, Action 1"
+            }
+            doFirst {
+                println "execution phase, Action 2"
+            }
+            doLast {
+                println "execution phase, Action 3"
+            }
+            println "task: $description, group: $group, actions: ${actions.size()}"
+            println "path task: $path"
+        }
+    }
+}
