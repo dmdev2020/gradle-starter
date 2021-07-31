0a5712a10b1c8d375497d3f4653ffece21b85c2a
dmdev2020 <dmdev2020@gmail.com>
2021-07-31 17:05:25 +0200

    added classes for lesson-29

---
+++ service/src/test/java/com/dmdev/service/UserServiceTest.java
@@ -0,0 +1,19 @@
+package com.dmdev.service;
+
+import com.dmdev.model.User;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    private final UserService userService = new UserService();
+
+    @Test
+    void getAll() {
+        List<User> actual = userService.getAll();
+        assertEquals(3, actual.size());
+    }
+}
